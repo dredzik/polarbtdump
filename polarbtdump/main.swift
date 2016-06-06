8c66c93fdf9bbc38440ac2beed0177b2de88d8d4
Adam Kuczyński <ak@niekoniecznie.pl>
2016-06-06 19:58:48 +0100

    Initial Commit

---
+++ polarbtdump/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  polarbtdump
+//
+//  Created by Adam Kuczyński on 06.06.2016.
+//  Copyright © 2016 Adam Kuczyński. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
