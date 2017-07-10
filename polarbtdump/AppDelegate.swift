8043b7884d54dc87bd4563ae21b56f37c5f706b4
Adam Kuczynski <a+github@typedef.io>
2017-07-10 17:15:35 +0100

    Split communication and dumping

--- polarbtdump/AppDelegate.swift
+++ polarbtdump/AppDelegate.swift
@@ -7,14 +7,116 @@
 //
 
 import Cocoa
+import CoreBluetooth
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var dumper: Dumper!
+class AppDelegate: NSObject, NSApplicationDelegate, CBCentralManagerDelegate, CBPeripheralManagerDelegate, DumperDelegate {
+
+    var centralManager: CBCentralManager?
+    var peripheralManager: CBPeripheralManager?
+    let service = CBMutableService(type: Constants.UUIDs.Service, primary: true)
+    let data = CBMutableCharacteristic(type: Constants.UUIDs.Data, properties: Constants.Props, value: nil, permissions: Constants.Perms)
+
+    var dumper: Dumper?
+
+    // MARK: NSApplicationDelegate
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        dumper = Dumper()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+
+        service.characteristics = [data]
     }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
+    // MARK: CBCentralManagerDelegate
+    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            central.stopScan()
+        }
+    }
+
+    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        guard let name = peripheral.name else {
+            return
+        }
+
+        if !name.hasPrefix("Polar") || name.hasPrefix("Polar mobile") {
+            return
+        }
+
+        dumper = Dumper(device: peripheral, delegate: self)
+
+        central.stopScan()
+        central.connect(peripheral, options: nil)
+    }
+
+    public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        let notification = NSUserNotification()
+        notification.title = "Device connected"
+        notification.informativeText = peripheral.name
+        NSUserNotificationCenter.default.deliver(notification)
+    }
+
+    public func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        let notification = NSUserNotification()
+        notification.title = "Device disconnected"
+        notification.informativeText = peripheral.name
+        NSUserNotificationCenter.default.deliver(notification)
+
+        dumper = nil
+
+        central.scanForPeripherals(withServices: nil, options: nil)
+    }
+
+    // MARK: CBPeripheralManagerDelegate
+    public func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        if peripheral.state == .poweredOn {
+            peripheral.add(service)
+        } else {
+            peripheral.removeAllServices()
+        }
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+        peripheral.startAdvertising([
+            CBAdvertisementDataServiceUUIDsKey : Constants.UUIDs.Service,
+            CBAdvertisementDataLocalNameKey : "Polar mobile 666",
+            CBAdvertisementDataIsConnectable : true,
+        ])
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        peripheral.setDesiredConnectionLatency(.low, for: central)
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
+        peripheral.respond(to: request, withResult: .success)
+        data.value = Data([0x0f, 0x00])
+
+        dumper?.dump()
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite request: CBATTRequest) {
+        peripheral.respond(to: request, withResult: .success)
+        if let value = request.value {
+            data.value = value
+            dumper?.recvPacket(Array(value))
+        }
+    }
+
+    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        for request in requests {
+            self.peripheralManager(peripheral, didReceiveWrite: request)
+        }
+    }
+
+    public func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
+        dumper?.sendPacket()
+    }
+
+    // MARK: DumperDelegate
+    public func updateValue(_ value: Data) -> Bool {
+        return peripheralManager!.updateValue(value, for: data, onSubscribedCentrals: nil)
     }
 }
--- polarbtdump/Dumper.swift
+++ polarbtdump/Dumper.swift
@@ -9,130 +9,32 @@
 import CoreBluetooth
 import Foundation
 
-public class Dumper: NSObject, CBCentralManagerDelegate, CBPeripheralManagerDelegate {
-    
-    var centralManager: CBCentralManager?
-    var peripheralManager: CBPeripheralManager?
-    var notificationCenter = NSUserNotificationCenter.default
+public protocol DumperDelegate {
 
-    var device: CBPeripheral?
+    func updateValue(_ value: Data) -> Bool
+}
 
-    let service = CBMutableService(type: Constants.UUIDs.Service, primary: true)
-    let data = CBMutableCharacteristic(type: Constants.UUIDs.Data, properties: Constants.Props, value: nil, permissions: Constants.Perms)
+public class Dumper: NSObject {
 
-    public override init() {
-        super.init()
-
-        service.characteristics = [data]
-
-        centralManager = CBCentralManager(delegate: self, queue: nil)
-        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
-    }
-    
-    // CBCentralManagerDelegate
-    public func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        if central.state == .poweredOn {
-            central.scanForPeripherals(withServices: nil, options: nil)
-        } else {
-            central.stopScan()
-        }
-    }
-
-    public func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        guard let name = peripheral.name else {
-            return
-        }
-
-        if !name.hasPrefix("Polar") || name.hasPrefix("Polar mobile") {
-            return
-        }
-        
-        device = peripheral
-        
-        central.stopScan()
-        central.connect(peripheral, options: nil)
-    }
-
-    public func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
-        let notification = NSUserNotification()
-        notification.title = "Device connected"
-        notification.informativeText = peripheral.name
-        notificationCenter.removeAllDeliveredNotifications()
-        notificationCenter.deliver(notification)
-    }
-
-    public func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
-        let notification = NSUserNotification()
-        notification.title = "Device disconnected"
-        notification.informativeText = peripheral.name
-        notificationCenter.removeAllDeliveredNotifications()
-        notificationCenter.deliver(notification)
-
-        device = nil
-
-        central.scanForPeripherals(withServices: nil, options: nil)
-    }
-    
-    // CBPeripheralManagerDelegate
-    public func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        if peripheral.state == .poweredOn {
-            peripheral.add(service)
-        } else {
-            peripheral.removeAllServices()
-        }
-    }
-
-    public func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
-        peripheral.startAdvertising([
-            CBAdvertisementDataServiceUUIDsKey : Constants.UUIDs.Service,
-            CBAdvertisementDataLocalNameKey : "Polar mobile 666",
-            CBAdvertisementDataIsConnectable : true,
-            ])
-    }
-
-    public func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
-        peripheral.setDesiredConnectionLatency(.low, for: central)
-    }
-
-    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
-        peripheral.respond(to: request, withResult: .success)
-        data.value = Data([0x0f, 0x00])
-        
-        dump()
-    }
-
-    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite request: CBATTRequest) {
-        peripheral.respond(to: request, withResult: .success)
-        if let value = request.value {
-            data.value = value
-            recvPacket(Array(value))
-        }        
-    }
-    
-    public func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
-        for request in requests {
-            self.peripheralManager(peripheral, didReceiveWrite: request)
-        }
-    }
-
-    public func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
-        sendPacket()
-    }
-    
-    // Dumper
+    var delegate: DumperDelegate
+    var device: CBPeripheral
     var dumpCurrent: String?
     var dumpPaths = [String]()
     var recvChunks = [PSChunk]()
     var recvPackets = [PSPacket]()
     var sendChunks = [PSChunk]()
     var sendPackets = [[UInt8]]()
+
+    init(device: CBPeripheral, delegate: DumperDelegate) {
+        self.device = device
+        self.delegate = delegate
+    }
     
     func dump() {
         let notification = NSUserNotification()
         notification.title = "Sync started"
-        notification.informativeText = device?.name
-        notificationCenter.removeAllDeliveredNotifications()
-        notificationCenter.deliver(notification)
+        notification.informativeText = device.name
+        NSUserNotificationCenter.default.deliver(notification)
         sendRaw(Constants.Packets.SyncBegin)
 
         dumpPaths.append("/U/0/")
@@ -143,9 +45,8 @@ public class Dumper: NSObject, CBCentralManagerDelegate, CBPeripheralManagerDele
         if dumpPaths.count == 0 {
             let notification = NSUserNotification()
             notification.title = "Sync finished"
-            notification.informativeText = device?.name
-            notificationCenter.removeAllDeliveredNotifications()
-            notificationCenter.deliver(notification)
+            notification.informativeText = device.name
+            NSUserNotificationCenter.default.deliver(notification)
 
             sendRaw(Constants.Packets.SyncEnd)
             sendRaw(Constants.Packets.SessionEnd)
@@ -182,7 +83,7 @@ public class Dumper: NSObject, CBCentralManagerDelegate, CBPeripheralManagerDele
     
     public func sendPacket() {
         while sendPackets.count > 0 {
-            if !peripheralManager!.updateValue(Data(sendPackets[0]), for: data, onSubscribedCentrals: nil) {
+            if !delegate.updateValue(Data(sendPackets[0])) {
                 break
             }
             
