230015ce0f6ddd950ea721959c390c99905520c9
Adam Kuczynski <a+github@typedef.io>
2017-07-13 12:08:14 +0100

    Cleaner way to process user notifications

--- polarbtdump/AppDelegate.swift
+++ polarbtdump/AppDelegate.swift
@@ -17,48 +17,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         deviceManager = DeviceManager()
 
-        NotificationCenter.default.addObserver(forName: PBTDNDeviceConnected, object: nil, queue: nil, using: self.notificationDeviceConnected)
-        NotificationCenter.default.addObserver(forName: PBTDNDeviceDisconnected, object: nil, queue: nil, using: self.notificationDeviceDisconnected)
-        NotificationCenter.default.addObserver(forName: PBTDNSyncStarted, object: nil, queue: nil, using: self.notificationSyncStarted)
-        NotificationCenter.default.addObserver(forName: PBTDNSyncFinished, object: nil, queue: nil, using: self.notificationSyncFinished)
+        NotificationCenter.default.addObserver(forName: PBTDNDeviceConnected, object: nil, queue: nil, using: self.notifyUser)
+        NotificationCenter.default.addObserver(forName: PBTDNDeviceDisconnected, object: nil, queue: nil, using: self.notifyUser)
+        NotificationCenter.default.addObserver(forName: PBTDNSyncStarted, object: nil, queue: nil, using: self.notifyUser)
+        NotificationCenter.default.addObserver(forName: PBTDNSyncFinished, object: nil, queue: nil, using: self.notifyUser)
     }
 
-    func notificationDeviceConnected(_ aNotification: Notification) {
+    func notifyUser(_ aNotification: Notification) {
+        var message = ""
+
+        switch aNotification.name {
+        case PBTDNDeviceConnected:
+            message = "Device connected"
+        case PBTDNDeviceDisconnected:
+            message = "Device disconnected"
+        case PBTDNSyncStarted:
+            message = "Sync started"
+        case PBTDNSyncFinished:
+            message = "Sync finished"
+        default:
+            return
+        }
+
         guard let device = aNotification.object as? Device else {
             return
         }
 
-        deliver(title: "Device connected", informativeText:  device.name)
-    }
-
-    func notificationDeviceDisconnected(_ aNotification: Notification) {
-        guard let device = aNotification.object as? Device else {
-            return
-        }
-
-        deliver(title: "Device disconnected", informativeText: device.name)
-    }
-
-    func notificationSyncStarted(_ aNotification: Notification) {
-        guard let device = aNotification.object as? Device else {
-            return
-        }
-
-        deliver(title: "Sync started", informativeText: device.name)
-    }
-
-    func notificationSyncFinished(_ aNotification: Notification) {
-        guard let device = aNotification.object as? Device else {
-            return
-        }
-
-        deliver(title: "Sync finished", informativeText: device.name)
-    }
-
-    func deliver(title: String, informativeText: String) {
         let notification = NSUserNotification()
-        notification.title = title
-        notification.informativeText = informativeText
+        notification.title = device.name
+        notification.informativeText = message
         NSUserNotificationCenter.default.deliver(notification)
     }
 }
